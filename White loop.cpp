63a1598f1d55d7421df1fba28f4ebcecbb0e2ff6
Catungal Kerwin Jan B. <qkjbcatungal@tip.edu.ph>
2024-09-16 12:36:09 +0800

    Add files via upload

---
+++ White loop.cpp
@@ -0,0 +1,31 @@
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+	
+	int i=10;
+	
+	while (i>0)
+	
+	{
+		cout<<"Hello"<<i<<endl;
+		i=i-1;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ for loop.cpp
@@ -0,0 +1,50 @@
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+	//Data Type
+	int i=0;
+	int j=10;
+	
+	for(i-0;i<=j;i++)
+	{
+		
+        cout<<"Hello"<<i<<endl;
+		i=i+0;
+	}
+	return 0;
+   
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
