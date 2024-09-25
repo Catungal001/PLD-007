fca00ca9d306dcc798c808a6c5b0344dedd26fa7
Catungal Kerwin Jan B. <qkjbcatungal@tip.edu.ph>
2024-09-25 12:21:33 +0800

    Add files via upload

---
+++ gender.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	//create a c++ program that indentify if its male or female
+	char c;
+	
+	cout<<"Enter the first letter of the type of hobbies:";
+	cin>>c;
+	
+	switch(c)
+	{
+		
+		case 'cars': 
+			cout<<"male";
+		break;
+		
+		case 'a':
+			cout<<"male";
+		break;
+		
+		case 'b':
+			cout<<"male";
+		break;
+		
+		case 'f':
+			cout<<"male";
+		break;
+		
+		case 'd': 
+			cout<<"female";
+		break;
+		
+		case 'g':
+			cout<<"female";
+		break;
+		
+		case 'y':
+			cout<<"female";
+		break;
+		
+		case 'm':
+			cout<<"female";
+		break;
+
+        
+		   {
+		   	  cout<<"male";
+		      break;
+		      
+			  cout<<"female";
+		      break;
+		   }	
+
+
+
+	}
+
+    return 0;
+    
+
+
+}	
---
+++ standard calculator.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	//create a c++ program that apply a MDAS
+	char c;
+	
+	cout<<"Enter the Calculator:";
+	cin>>c;
+	
+	switch(c)
+	{
+		case '+': 
+			cout<<"addition";
+		break;
+		
+		case '-':
+			cout<<"subtraction";
+		break;
+		
+		case '*':
+			cout<<"multiplication";
+		break;
+		
+		case '/':
+			cout<<"devision";
+		break;
+		
+	
+		     
+								
+		default:
+		   {
+		   	  cout<<"not a calculator funtion";
+		      break;
+		   }	
+		
+		
+		
+		
+		
+	}
+	
+	
+	return 10;
+	
+	
+	
+
+}	
