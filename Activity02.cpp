03c1b01d94f70865a8e354d51ab017d1acb99baf
Catungal Kerwin Jan B. <qkjbcatungal@tip.edu.ph>
2024-10-07 13:19:04 +0800

    Add files via upload

---
+++ Activity02.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age, originalFare;
+	double discount;
+	
+	cout<<"Welcome!"
+	<<endl;
+	cout<<"Please enter your age maam/sir:";
+	cin>> age;
+	
+	if (age >= 60)
+	{
+		discount = 0.10;
+		cout<<"Congratualation! you qualify for our senior citizen discount"
+		<<discount << endl;
+	}
+	else if (age <= 25)
+	{
+		discount = 0.08;
+		cout<< "You qualify for a student discount"
+		<<discount <<endl;
+	}
+	else
+	{
+		cout<<"Unfortunately, you're not qualified for a discount."
+	    <<endl;
+	}
+	return 0;
+	
+	
+	
+	
+	
+	
+}
